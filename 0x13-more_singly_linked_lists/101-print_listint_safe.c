0f209297e03c63cf168fe3cbbb6e4968c1d4bc52
Webwithjoe <webwithjoe@gmail.com>
2023-05-02 21:52:35 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,5 +1,10 @@
 #include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
+
+/*func declaration*/
+size_t print_listint_safe(const listint_t *head);
+size_t looped_listint_len(const listint_t *head);
 
 /**
  * print_listint_safe - prints a listint_t linked list,
@@ -9,32 +14,78 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-const listint_t *slow, *fast;
-	size_t mycount = 0;
+size_t mynodes, index = 0;
 
-	if (head == NULL)
-	exit(98);
+	mynodes = looped_listint_len(head);
 
-	slow = head;
-	fast = head->next;
-
-	while (fast != NULL && fast < fast->next)
+	if (mynodes == 0)
 	{
-	printf("[%p] %d\n", (void *)slow, slow->n);
-	mycount++;
-
-	slow = slow->next;
-	fast = fast->next->next;
+		for (; head != NULL; mynodes++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
 	}
 
-	printf("[%p] %d\n", (void *)slow, slow->n);
-	mycount++;
-
-	if (fast != NULL)
+	else
 	{
-	printf("-> [%p] %d\n", (void *)fast, fast->n);
-	mycount++;
+		for (index = 0; index < mynodes; index++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
+
+		printf("-> [%p] %d\n", (void *)head, head->n);
 	}
 
-	return (mycount);
+	return (mynodes);
+}
+
+/**
+ * looped_listint_len - FUNC Counts number of unique nodes
+ *                      in  looped listint_t linked list.
+ * @head: Pointer to the head of listint_t to check.
+ *
+ * Return: When list is not looped - 0.
+ *         Otherwise - the num of unique nodes in list.
+ */
+size_t looped_listint_len(const listint_t *head)
+{
+	/* Tortoise...Hare Algorithm */
+	const listint_t *tortoise, *hare;
+	size_t mynodes = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	tortoise = head->next;
+	hare = (head->next)->next;
+
+	while (hare)
+	{
+		if (tortoise == hare)
+		{
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				mynodes++;
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+
+			tortoise = tortoise->next;
+			while (tortoise != hare)
+			{
+				mynodes++;
+				tortoise = tortoise->next;
+			}
+
+			return (mynodes);
+		}
+
+		tortoise = tortoise->next;
+		hare = (hare->next)->next;
+	}
+
+	return (0);
 }
