0e5b43aa8867fb5be40515cdda8487eb2458c001
Webwithjoe <webwithjoe@gmail.com>
2023-05-05 19:58:06 -0700

    update

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H /* preprocessor directive for if define */
 
 
-
+int _putchar(char c)
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
